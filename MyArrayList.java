d80f3f70d4c26eaea648643c866e44e3abccb366
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-14 21:39:14 +0600

    Create MyArrayList.java

---
+++ MyArrayList.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class MyArrayList<T> implements List<T> {
+
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = oldCapacity * 2;
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+}
