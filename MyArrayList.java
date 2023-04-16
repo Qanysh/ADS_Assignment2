37dd883e8b44adbaad2eff4a675bddf9ef55fcf9
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-16 20:06:13 +0600

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Iterator;
 
-public abstract class MyArrayList<E> implements MyList<E> {
+public class MyArrayList<E> implements MyList<E> {
 
     private Object[] elements;
     private int size;
@@ -11,44 +11,109 @@ public abstract class MyArrayList<E> implements MyList<E> {
         size = 0;
     }
 
-    private void ensureCapacity(int minCapacity) {
-        int oldCapacity = elements.length;
-        if (minCapacity > oldCapacity) {
-            int newCapacity = oldCapacity * 2;
-            if (newCapacity < minCapacity)
-                newCapacity = minCapacity;
-            elements = Arrays.copyOf(elements, newCapacity);
-        }
-    }
-
-    public void add(E element) {
-        ensureCapacity(size + 1);
-        elements[size++] = element;
-    }
-
-    @SuppressWarnings("unchecked")
-    public E get(int index) {
-        if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException();
-        return (E) elements[index];
-    }
-
-    public E remove(int index) {
-        if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException();
-        @SuppressWarnings("unchecked")
-        E oldValue = (E) elements[index];
-        int numMoved = size - index - 1;
-        if (numMoved > 0)
-            System.arraycopy(elements, index + 1, elements, index, numMoved);
-        elements[--size] = null;
-        return oldValue;
-    }
-
     public int size() {
         return size;
     }
 
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(E item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        if (size == elements.length) {
+            resize();
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = item;
+        size++;
+    }
+
+    public void add(E item) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size] = item;
+        size++;
+    }
+
+    public boolean remove(E item) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(item)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        E removed = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return removed;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        return (E) elements[index];
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void sort() {
+        Arrays.sort(elements, 0, size);
+    }
+
+    private void resize() {
+        Object[] newElements = new Object[elements.length * 2];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+    }
+
+    @Override
     public Iterator<E> iterator() {
         return new MyIterator();
     }
@@ -67,4 +132,3 @@ public abstract class MyArrayList<E> implements MyList<E> {
         }
     }
 }
-
